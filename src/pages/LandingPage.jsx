b325a5a45a7487f0c4e4fda68fc9aaf30a6beb00
RAJENDRA YADAV <rjyadav0230@gmail.com>
2023-03-29 14:38:37 +0530

    Featute component completed

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,13 @@
 import { Box } from "@chakra-ui/react";
 import React from "react";
 import CreativeProxyBox from "../LandPageComponents/CreativeProxyBox";
+import Feature from "../LandPageComponents/Feature";
 
 const LandingPage = () => {
   return (
     <Box w="100%">
       <CreativeProxyBox />
+      <Feature />
     </Box>
   );
 };
---
+++ src/utils/data.js
@@ -0,0 +1,59 @@
+import featuer1 from "../assets/people.svg";
+import featuer2 from "../assets/cycle.svg";
+import featuer3 from "../assets/devices.svg";
+import featuer4 from "../assets/messages.svg";
+import featuer5 from "../assets/globe.svg";
+import featuer6 from "../assets/shield-tick.svg";
+import featuer7 from "../assets/trade.svg";
+import featuer8 from "../assets/flash.svg";
+
+export const featureData = [
+  {
+    id: 1,
+    Image: featuer1,
+    title: "Feature No 1",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies interdum eros non suscipit.",
+  },
+  {
+    id: 2,
+    Image: featuer2,
+    title: "Feature No 2",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies interdum eros non suscipit.",
+  },
+  {
+    id: 3,
+    Image: featuer3,
+    title: "Feature No 3",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies interdum eros non suscipit.",
+  },
+  {
+    id: 4,
+    Image: featuer4,
+    title: "Feature No 4",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies interdum eros non suscipit.",
+  },
+  {
+    id: 5,
+    Image: featuer5,
+    title: "Feature No 5",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies interdum eros non suscipit.",
+  },
+  {
+    id: 6,
+    Image: featuer6,
+    title: "Feature No 6",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies interdum eros non suscipit.",
+  },
+  {
+    id: 7,
+    Image: featuer7,
+    title: "Feature No 7",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies interdum eros non suscipit.",
+  },
+  {
+    id: 8,
+    Image: featuer8,
+    title: "Feature No 8",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies interdum eros non suscipit.",
+  },
+];
